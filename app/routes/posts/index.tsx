7ee7597b0b49c100b52aa4c5c649bb1e66101e34
AntwanSherif <antwansherif@gmail.com>
2023-01-25 17:10:05 +0200

    :sparkles: Fetch data using loaders

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,36 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+
+export const loader = async () =>
+  json({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  });
+
 export default function PostsRoute() {
+  const { posts } = useLoaderData();
+
   return (
     <main>
       <h1>Posts</h1>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
