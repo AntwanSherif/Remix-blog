e050ec2b83fc3ab44f2e5f451b246ed41f69ce53
AntwanSherif <antwansherif@gmail.com>
2023-01-25 16:58:45 +0200

    :sparkles: Create our first Remix route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -63,6 +63,12 @@ export default function Index() {
           </div>
         </div>
 
+        <div className="mx-auto mt-16 max-w-7xl text-center">
+          <Link to="/posts" className="text-xl text-blue-600 underline">
+            Blog Posts
+          </Link>
+        </div>
+
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
