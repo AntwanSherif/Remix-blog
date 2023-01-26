7217fc4fd60437a2706a72129986aef948f102d4
AntwanSherif <antwansherif@gmail.com>
2023-01-26 19:20:12 +0200

    :recycle: Fetch data with prisma

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,5 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import { prisma } from "~/db.server";
 
-export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+export async function getPosts() {
+  return prisma.post.findMany();
 }
